5cdb4be1b7ada973a3b0f3334b4eb23272027c9d
lesimoes <lesimoes0@gmail.com>
2022-06-09 16:55:26 -0300

    added sale and featured collections

--- src/components/CollectionGallery/CollectionGallery.js
+++ src/components/CollectionGallery/CollectionGallery.js
@@ -1,15 +1,27 @@
 import { Link } from 'gatsby';
 import React, { useContext } from 'react';
 import ProductContext from '../../context/ProductContext';
-import { CollectionWrapper } from './styles';
+import { CollectionWrapper, SaleContainer } from './styles';
 
 export default function CollectionGallery () {
 
     const { collections } = useContext(ProductContext);
 
+    //TODO Change this handle - sala
+    const featuredCollections = collections.filter(value => value.handle !== 'sala');
+    const saleCollection = collections.filter(value => value.handle === 'sala');
+
     return (
         <CollectionWrapper>
-            { collections.map(({title, shopifyId, image, handle}) => (
+            { saleCollection.map(({ title, shopifyId, image, handle}) => 
+                <SaleContainer>
+                    <Link to={`/collections/${handle}`}>
+                        <img src={image.originalSrc} alt={title} key={shopifyId}/>
+                        <h5>{title}</h5>
+                    </Link>
+                </SaleContainer>
+            )}
+            { featuredCollections.map(({title, shopifyId, image, handle}) => (
                 <div>
                     <Link to={`/collections/${handle}`}>
                         <img src={image.originalSrc} alt={title} key={shopifyId}/>
--- src/components/CollectionGallery/styles.js
+++ src/components/CollectionGallery/styles.js
@@ -3,15 +3,52 @@ import styled from 'styled-components';
 export const CollectionWrapper = styled.section`
 
     display: flex;
-    justify-content: space-around;
-
+    justify-content: space-between;
+    flex-wrap: wrap;
+    
     img {
-        width: 320px;
+        width: 380px;
         height: 220px;
         object-fit: cover;
+        opacity: 1;
+        border: 1px solid gray
     }
 
     img:hover {
         cursor: pointer;
+        transform: scale(1.1);
+        transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+        opacity: 0.6;
+        border: 1px solid gray
     }
+
+    div {
+        position: relative;
+    }
+
+    h5 {
+        position: absolute;
+        top: calc(50% - 40px);
+        left: calc(50% - 40px);
+        font-size: 32px;
+        font-weight: bold;
+        color: white;
+    }
+    
+`
+
+export const SaleContainer = styled.div`
+    width: 100%;
+    display: block;
+    position: relative;
+
+    img {
+        width: 100%;   
+    }
+
+    h5 {
+        font-size: 52px;
+        color: black;
+    }
+
 `
